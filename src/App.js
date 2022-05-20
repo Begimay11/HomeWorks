03ed02ca070c10d3f2df7ad4a7ca7855e851617c
Begimay11 <begimaysagynbaeva245@gmail.com>
2022-05-20 12:15:01 +0600

    hw11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [userList, setUserList] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList(prevUserList => {
+      return[...prevUserList, {name: uName, age: uAge, id: Math.random().toString()}]
+    })
+  }
+
+  function getId(id){
+    let newUserList = userList.filter(user=>{
+      return user.id !== id;
+    })
+    setUserList(newUserList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AddUser onAddUser= {addUserHandler} />
+       <UsersList users = {userList} setUserList={setUserList} getId={getId} />
     </div>
   );
 }
+
 
 export default App;
